7222695698b0214b5d950f44335102d65c499ce3
dom-xbr <dmaxbrown@outlook.com>
2023-03-17 15:27:00 +0000

    v1

--- source/react-links/src/webparts/links/loc/en-us.js
+++ source/react-links/src/webparts/links/loc/en-us.js
@@ -1,25 +1,26 @@
-define([], function() {
+define([], function () {
   return {
-    "groupDataLabel": "Group names for the links",
-    "groupPanelHeader": "Configure your group names",
-    "manageGroupBtn": "Configure groups",
-    "iconInformation": "The names to specify for the icon field can be found on the following URL:",
+    groupDataLabel: "Group names for the links",
+    groupPanelHeader: "Configure group names",
+    manageGroupBtn: "Configure groups",
+    iconInformation:
+      "The names to specify for the icon field can be found on the following URL:",
 
-    "linkDataLabel": "Link data",
-    "linkPanelHeader": "Configure your links",
-    "manageLinksBtn": "Configure links",
+    linkDataLabel: "Link data",
+    linkPanelHeader: "Configure your links",
+    manageLinksBtn: "Configure links",
 
-    "titleField": "Title",
-    "urlField": "URL",
-    "iconField": "UI Fabric icon name",
-    "groupField": "Group name",
-    "targetField": "Target",
+    titleField: "Title",
+    urlField: "URL",
+    iconField: "UI Fabric icon name",
+    groupField: "Group name",
+    targetField: "Target",
 
-    "targetCurrent": "Current window",
-    "targetNew": "New tab",
+    targetCurrent: "Current window",
+    targetNew: "New tab",
 
-    "noLinksIconText": "Configure your links",
-    "noLinksConfigured": "Please configure the web part in order to show links",
-    "noLinksBtn": "Configure",
-  }
+    noLinksIconText: "Configure your links",
+    noLinksConfigured: "Please configure the web part in order to show links",
+    noLinksBtn: "Configure",
+  };
 });
